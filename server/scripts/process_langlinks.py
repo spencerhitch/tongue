714b9d2659961bd1c99d5f550931d3e6fe42db8c
Spencer Hitchcock <spencerhitch@gmail.com>
2016-08-20 17:58:27 -0700

    process_langlinks script now properly generates every title of an English language article which has a spanish version and the title of it's spanish version

--- server/scripts/process_langlinks.py
+++ server/scripts/process_langlinks.py
@@ -1,11 +1,38 @@
 import sqlite3
-with open("../data/enwiki-20160801-langlinks.sql", 'rb') as db:
-    db = db.read()
-    print(db[0:1000])
+import sys
+
+page_items = {}
+es_items = {}
+result = {}
+
+decoded = ""
+
+with open("../data/enwiki-20160801-page.sql", 'rb') as page_db:
+    for line in page_db:
+        decoded = decoded + line.decode('iso-8859-1')
+    for w in decoded.split("),("):
+        w_split = w.split(',')
+        if len(w_split) >= 3:
+            page_items[w_split[0]] = w_split[2]
+
+print(list(page_items)[0:100])
+
+with open("../data/enwiki-20160801-langlinks.sql", 'rb') as lang_db:
+    lang_db = lang_db.read().decode('iso-8859-1')
+    for w in lang_db.split("),("):
+        if "'es'" in w and "Categor" not in w:
+            w_split = w.split(',')
+            if len(w_split) >= 3:
+                es_items[w_split[0]] = w_split[2]
+
+print(list(es_items)[0:100])
+
+for key in es_items.keys():
+    try:
+        result[[page_items[key]] =  es_items[key]
+    except:
+        continue
+
+print(result)
 
 
-#    connection = sqlite3.connect(db)
-#    cursor = connection.cursor()
-#    cursor.execute("SELECT table_name FROM all_tables")
-#    print("fetchall:")
-#    result = cursor.fetchall()
