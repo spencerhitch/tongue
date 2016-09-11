33c98ae651d498a45b76fe77d71422f21ca9021f
Spencer Hitchcock <spencerhitch@gmail.com>
2016-09-10 20:01:32 -0700

    Succesfully souping out the content of a given article.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import nltk, urllib
 from nltk import word_tokenize
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -11,6 +12,11 @@ def index():
 @app.route('/addArticle', methods=['POST'])
 def addArticle():
   title = (request.form['title'])
-  print(title)
+  url = 'http://en.wikipedia.org/wiki/' + title
+  html = urllib.request.urlopen(url).read().decode('utf8')
+  soup = BeautifulSoup(html)
+  content = soup.find(id='mw-content-text').find_all('p')
+  for p in content:
+      print(p.getText())
   return "OK"
 
